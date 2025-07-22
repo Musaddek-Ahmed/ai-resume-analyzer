07e824dbdf9a5428f88c0189ee437a2216ffebf7
Musaddek-Ahmed <musaddek59@gmail.com>
2025-07-22 13:50:41 +0600

    puter.js setup done for auth

--- app/components/resumeCard.tsx
+++ app/components/resumeCard.tsx
@@ -19,7 +19,7 @@ const ResumeCard = ({resume: { id, companyName, jobTitle, feedback, imagePath }}
           <img
             src={imagePath}
             alt='resume'
-            className=' w-full h-[350px] max-sm:h-[200px] object-cover object-top'
+            className=' w-full h-[350px] max-sm:h-[350px] object-cover object-top rounded-md'
           />
         </div>
       </div>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import Navbar from "~/components/navbar";
 import type { Route } from "./+types/home";
 import ResumeCard from "~/components/resumeCard";
 import { resumes } from "../../constants";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -12,24 +15,36 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth} = usePuterStore();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      if(!auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated])
+
   return  <main className="bg-[url('/images/bg-main.svg')] bg-cover">
 
     <Navbar />
 
     <section className="main-section">
-      <div className="page-heading">
-        <h1>Track You Applications & Resume Ratings</h1>
+      <div className="page-heading py-16">
+        <h1>Track Your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback</h2>
       </div>
-    </section>
 
-    {resumes.length > 0 && (
+      {resumes.length > 0 && (
       <div className="resumes-section">
         {resumes.map((resume) => (
           <ResumeCard key={resume.id} resume={resume}/>
         ))}
       </div>
     )}
+      </section>
+    
+
+    
+
+    
 
 
   </main>
