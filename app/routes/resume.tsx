14748a60810fbf56013d67e7a4eb14a65e33a151
Musaddek-Ahmed <musaddek59@gmail.com>
2025-07-27 20:20:21 +0600

    resume page done

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -51,7 +51,7 @@ const Resume = () => {
     }, [id])
 
   return (
-    <main className='!pt-0'>
+    <main className='!pt-0 '>
         <nav className='resume-nav'>
             <Link to='/' className='back-button'>
                 <img src="/icons/back.svg" alt="logo" className='w-2.5 h-2.5' />
@@ -60,7 +60,7 @@ const Resume = () => {
         </nav>
 
         <div className='flex flex-row w-full max-lg:flex-col-reverse'>
-            <section className='feedback-section bg-[url("/images/bg-small.svg")] bg-cover h-[100vh] sticky top-0 items-center justify-center'>
+            <section className='feedback-section bg-[url("/images/bg-small.svg")] bg-cover h-[90vh] sticky top-0 items-center justify-center'>
                 {imageUrl && resumeUrl && (
                     <div className='animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit'>
                         <a href={resumeUrl} target="_blank" rel="noopener noreferrer" >
@@ -70,15 +70,15 @@ const Resume = () => {
                 )}
             </section>
 
-            <section className='feedback-section'>
+            <section className='feedback-section h-[90vh] overflow-y-auto'>
                 <h2 className='text-4xl !text-black font-bold '>Resume Review</h2>
                 
                 {
                     feedback ? (
                         <div className='flex flex-col gap-8 animate-in fade-in duration-1000'>
                             <Summary feedback={feedback} />
-                            {/* <ATS score={feedback.ATS.score || 0} suggestions={feedback.padStart.tips || []}/>
-                            <Details feedback={feedback} /> */}
+                            <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []}/>
+                             <Details feedback={feedback} /> 
                         </div>
                     ) : (
                         <div>
